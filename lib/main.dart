6d46bd3cdb6647f8c3539a29f21ccd70c1598263
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-11-29 15:33:28 +0800

    feat: Passing Data to Stareful Widgets.

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('EasyList Show AppBar'),
         ),
-        body: ProductManager(),
+        body: ProductManager('Food Tester'),
       ),
     );
   }
--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import './products.dart';
 
 class ProductManager extends StatefulWidget {
+  final String startingProduct;
+  ProductManager(this.startingProduct);
   @override
   State<StatefulWidget> createState() {
     return _ProductManagerState();
@@ -10,7 +12,13 @@ class ProductManager extends StatefulWidget {
 }
 
 class _ProductManagerState extends State<ProductManager> {
-  List<String> _products = ['Food Bar'];
+  List<String> _products = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _products.add(widget.startingProduct);
+  }
 
   @override
   Widget build(BuildContext context) {
