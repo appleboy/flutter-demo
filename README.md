4da30878b015eb7d1ec57a62820fdb9e58b2a5c3
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-11-29 11:43:23 +0800

    feat: initial

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # flutter-demo
+
 how to write android and ios once using flutter
+
+A new Flutter project.
+
+## Getting Started
+
+For help getting started with Flutter, view our online [documentation](https://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('EasyList Show AppBar'),
+        ),
+        body: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                onPressed: () {},
+                child: Text('Add Product'),
+              ),
+            ),
+            Card(
+              child: Column(
+                children: <Widget>[
+                  Image.asset('assets/kubeflow.jpg'),
+                  Text('Food Paradise'),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
