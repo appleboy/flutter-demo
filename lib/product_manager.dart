18598807c80b83d3449b00dac7dd22edc6f43a05
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-11-30 10:15:27 +0800

    feat: initState() and super.initState()

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -4,9 +4,12 @@ import './products.dart';
 
 class ProductManager extends StatefulWidget {
   final String startingProduct;
-  ProductManager(this.startingProduct);
+  ProductManager(this.startingProduct){
+    print('[Products Manager] Constructor');
+  }
   @override
   State<StatefulWidget> createState() {
+    print('[Products Manager] State');
     return _ProductManagerState();
   }
 }
@@ -17,11 +20,19 @@ class _ProductManagerState extends State<ProductManager> {
   @override
   void initState() {
     super.initState();
+    print('[Products Manager State] initState');
     _products.add(widget.startingProduct);
   }
 
   @override
+  void didUpdateWidget(ProductManager oldWidget) {
+    print('[Products Manager State] didUpdateWidget()');
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print('[Products Manager State] build');
     return Column(
       children: [
         Container(
--- lib/products.dart
+++ lib/products.dart
@@ -2,16 +2,20 @@ import 'package:flutter/material.dart';
 
 class Products extends StatelessWidget {
   final List<String> products;
-  Products(this.products);
+  Products(this.products) {
+    print('[Products Widget] Constructor');
+  }
   @override
   Widget build(BuildContext context) {
+    print('[Products Widget] build()');
+
     return Column(
         children: products
             .map(
               (element) => Card(
                     child: Column(
                       children: <Widget>[
-                        Image.asset('assets/kubeflow.jpg'),
+                        // Image.asset('assets/kubeflow.jpg'),
                         Text(element),
                       ],
                     ),
